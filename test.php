b6b0de240a99310164dbef24bcc4dc7687849b4e
mrok5 <6220classic@gmail.com>
2015-11-05 17:35:39 +0100

    usunięto niepotrzebny plik

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'dbconfig.php';
-
-$stmt = $DB_con->prepare("SELECT id, nazwa FROM albumy");
-				$stmt->execute();
-				
-				while ($tabela = $stmt->fetch()):
-					echo '<option value="'.$tabela['id'].'">'.$tabela['nazwa'].'</option>'; 	
-			   endwhile;
