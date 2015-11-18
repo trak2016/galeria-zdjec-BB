63c67430808aa0c5e7b7727bdd9349262f56083f
mrok5 <6220classic@gmail.com>
2015-11-18 05:34:07 +0100

    update bazy danych

--- baza_danych/dblogin.sql
+++ baza_danych/dblogin.sql
@@ -26,18 +26,21 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `albumy`
 --
 
-CREATE TABLE `albumy` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nazwa` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `albumy` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nazwa` varchar(20) NOT NULL,
+  `id_usera` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`,`id_usera`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Zrzut danych tabeli `albumy`
 --
 
-INSERT INTO `albumy` (`id`, `nazwa`) VALUES
-(3, 'test'),
-(4, 'test2');
+INSERT INTO `albumy` (`id`, `nazwa`, `id_usera`) VALUES
+(8, 'test1', 4),
+(9, 'test2', 4),
+(10, 'Nowy_albumer_user2', 5);
 
 -- --------------------------------------------------------
 
@@ -45,19 +48,24 @@ INSERT INTO `albumy` (`id`, `nazwa`) VALUES
 -- Struktura tabeli dla tabeli `users`
 --
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) NOT NULL,
   `user_email` varchar(60) NOT NULL,
-  `user_pass` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `user_pass` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Zrzut danych tabeli `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_pass`) VALUES
-(1, 'm', 'm@m.pl', '$2y$10$RQNEfUbfNG4f9EoYop1AyenwrNrFrC3fs/SA3/5A4/f7gxpEn/uWO');
+(4, 'm', 'm@m.pl', '$2y$10$O7obwb8DITqlZNz0B05rhOAqbtz4ZgA56Zbbif9qT5uWjenxKuE3q'),
+(5, 'user2', 'user2@user2.pl', '$2y$10$IT9VaQiYVRoWQHsVp6axWujkk3j1vdsRIxu/W2fNJYuJUNsK6aV8m'),
+(6, 'WIKARD', 'wikard@op.pl', '$2y$10$Qn/yBC0WFUyQIyoM1DPzTe8CszbhCJzyqof1R/Fu5RVIoFW1m7zda');
 
 -- --------------------------------------------------------
 
@@ -65,55 +73,24 @@ INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_pass`) VALUES
 -- Struktura tabeli dla tabeli `zdjecia`
 --
 
-CREATE TABLE `zdjecia` (
-  `id` int(11) NOT NULL,
-  `id_albumu` int(10) UNSIGNED NOT NULL,
-  `opis` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `zdjecia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_albumu` int(10) unsigned NOT NULL,
+  `id_usera` int(10) unsigned NOT NULL,
+  `nazwa` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`,`id_albumu`,`id_usera`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
--- Indeksy dla zrzutów tabel
+-- Zrzut danych tabeli `zdjecia`
 --
 
---
--- Indexes for table `albumy`
---
-ALTER TABLE `albumy`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `zdjecia` (`id`, `id_albumu`, `id_usera`, `nazwa`) VALUES
+(11, 8, 4, 'mmm'),
+(12, 8, 4, 'kwiatek'),
+(13, 9, 4, 'Pustynia'),
+(14, 10, 5, 'pingwiny');
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `user_name` (`user_name`),
-  ADD UNIQUE KEY `user_email` (`user_email`);
-
---
--- Indexes for table `zdjecia`
---
-ALTER TABLE `zdjecia`
-  ADD PRIMARY KEY (`id`,`id_albumu`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT dla tabeli `albumy`
---
-ALTER TABLE `albumy`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT dla tabeli `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT dla tabeli `zdjecia`
---
-ALTER TABLE `zdjecia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
